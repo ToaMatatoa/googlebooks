c1dadf91d8064c610e40ceb19c3b08f0b0adfdd4
Matatoa <rikonikotako@gmail.com>
2021-07-04 08:46:58 +0300

    Added Kodein DI to project, other different little changes

--- app/src/main/java/com/example/googlebooks/MainActivity.kt
+++ app/src/main/java/com/example/googlebooks/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.googlebooks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.KodeinTrigger
+import org.kodein.di.android.closestKodein
+import org.kodein.di.android.retainedKodein
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), KodeinAware {
+
+    private val parentKodein: Kodein by closestKodein()
+    override val kodein: Kodein by retainedKodein {
+        extend(parentKodein)
+    }
+    override val kodeinTrigger = KodeinTrigger()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        kodeinTrigger.trigger()
     }
 }
---
+++ app/src/main/java/com/example/googlebooks/di/UIModule.kt
@@ -0,0 +1,10 @@
+package com.example.googlebooks.di
+
+import org.kodein.di.Kodein
+
+object UIModule {
+
+    val uiModule = Kodein.Module("ui module", false) {
+
+    }
+}
