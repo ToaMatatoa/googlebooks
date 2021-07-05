ef9c8d1f8e18f907be182016b1a75d0813fb2ede
Matatoa <rikonikotako@gmail.com>
2021-07-05 15:44:47 +0300

    changed fragment books (RV and view model)

--- app/src/main/java/com/example/googlebooks/ui/FragmentBooks.kt
+++ app/src/main/java/com/example/googlebooks/ui/FragmentBooks.kt
@@ -1,8 +1,6 @@
 package com.example.googlebooks.ui
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.INVISIBLE
@@ -11,14 +9,15 @@ import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.googlebooks.R
 import com.example.googlebooks.databinding.FragmentBooksBinding
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.KodeinTrigger
 import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
 import org.kodein.di.generic.kcontext
-
 
 class FragmentBooks : Fragment(R.layout.fragment_books), KodeinAware {
 
@@ -31,6 +30,9 @@ class FragmentBooks : Fragment(R.layout.fragment_books), KodeinAware {
 
     private var _binding: FragmentBooksBinding? = null
     private val binding get() = _binding!!
+
+    private var adapterRV = Adapter()
+    private val viewModel: ViewModel by instance()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -49,6 +51,17 @@ class FragmentBooks : Fragment(R.layout.fragment_books), KodeinAware {
         binding.imgMenu.setOnClickListener {
             findNavController().navigate(R.id.action_fragmentBooks_to_fragmentSettings, null)
         }
+
+//        viewModel.getBooks()
+//        viewModel.liveDataRemoteProvider.observe(viewLifecycleOwner, Observer {
+//            adapterRV.addBooks(it)
+//        })
+
+        binding.rvList.apply {
+            layoutManager = LinearLayoutManager(context)
+            binding.rvList.adapter = adapterRV
+        }
+
 
         binding.etSearch.doOnTextChanged { text, _, _, _ ->
             binding.imgClose.visibility = if (text!!.isNotEmpty())
